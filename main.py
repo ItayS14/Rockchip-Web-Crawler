d8f3fb5d3598d331b3d9379f473e72ca551e6155
Itay Shirizly <it.shirizly@gmail.com>
2020-05-07 22:17:49 +0300

    Added cli with argparse

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 from RockchipCrawler import RockchipCrawler, RockchipFirmware
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+import argparse
+
+
+def validate_client(client):
+    """
+    The function will check if it the MongoClient is valid (was initialized with valid uri)
+    :param client: the mongodb client to check
+    :return: client is valid or not (bool)
+    """
+    try:
+        client.admin.command('ismaster') 
+    except ConnectionFailure:
+        return False
+    else:
+        return True
 
 def main():
-    crawler = RockchipCrawler()
-    client = MongoClient()
-    db = client.firmware_crawler
-    collection = db.rockchip_firmware_metadata
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server_url', help='The url to the website you wish to crawl')
+    parser.add_argument('-u', '--uri', help='Uri for the mongoDB', default='mongodb://localhost:27017/')
+    args = parser.parse_args()
     
+    client = MongoClient(args.uri, serverSelectionTimeoutMS=2)
+    if not validate_client(client):
+        print('error: MongoDB server not available')
+        exit()
+    db = client.firmware_crawler
+    
+    # Any new crawler will be added here with it's collection
+    supported_websites = {
+        'https://www.rockchipfirmware.com/': (RockchipCrawler(), db.rockchip_firmware_metadata)
+    }
+    
+    if args.server_url not in supported_websites.keys():
+        print(f'error: "{args.server_url}" website is not supported at the moment')
+        print('Supported websites:')
+        print('\n'.join(supported_websites.keys()))
+        exit()
+
+    crawler, collection = supported_websites[args.server_url]
+
     # Function that checks if the name exists in the database, needed for the crawler.get_firms to know which firmwares to yield
     exists = lambda name: collection.count_documents({'device_name': name}) > 0
-    
     for firm in crawler.get_firms(exists):
         collection.insert_one(firm.mongo_document)
         firm.download_files()
+
 
 if __name__ == '__main__':
     main()
